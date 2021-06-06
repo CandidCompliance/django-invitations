e4bdc6195bed8fccdd166d51a11830fd5105576f
Colin Byrne <co_byrne@yahoo.com>
2021-06-06 15:53:39 -0700

    vsadfa

--- invitations/migrations/0004_invitation_organization.py
+++ invitations/migrations/0004_invitation_organization.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import django.db.models.deletion
+from django.conf import settings
+from django.db import migrations, models
+
 class Migration(migrations.Migration):
 
     dependencies = [
